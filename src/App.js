49338f3fe9355aaf3bb2b5f26198a437afaac245
Srigadgeter <srigadgets@gmail.com>
2023-04-27 15:37:03 +0530

    fixed all lint issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Learn React
-    </div>
-  );
+  return <div className="App">Learn React</div>;
 }
 
 export default App;
