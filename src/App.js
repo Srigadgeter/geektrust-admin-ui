ff26ca75c55f91457be3dbd5ad0b569ea62a6d24
Srigadgeter <srigadgets@gmail.com>
2023-04-28 12:06:37 +0530

    Included the Users to the App comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+// external imports
 import React from "react";
+
+// internal imports
+import Users from "./pages/users/Users";
+
+// styles
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      <Users />
+    </div>
+  );
 }
 
 export default App;
